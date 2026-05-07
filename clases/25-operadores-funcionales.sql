7e9daa5609efa4c901547c9c3f73d5f03690dce8
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2026-05-06 19:19:54 -0600

    Add SQL examples for updating and deleting records, and introduce substring operations

---
+++ clases/25-operadores-funcionales.sql
@@ -0,0 +1,15 @@
+
+
+
+SELECT
+	id,
+	UPPER(name) AS upper_name, -- convierte los a mayusculas
+	LOWER(name) AS lower_name, -- convierte los a minisculas
+	LENGTH(name) as LENGTH, -- traer la longitud del nombre
+	(20 * 2) as constante, -- esto es una multiplicacion de constantes, no tiene sentido pero es para mostrar el uso de operadores
+	'*'||id||'-'|| name||'*' as barcode,
+	CONCAT( '*',id,'-', name,'*' ),-- esto concatena los valores, es lo mismo que la linea anterior
+	name -- name es el nombre original, esto es para mostrar que no se modifica el valor original
+FROM
+	users;
+
--- tarea/01-tarea.sql
+++ tarea/01-tarea.sql
@@ -1,18 +1,48 @@
 
 
 -- 1. Ver todos los registros
-
+SELECT
+	*
+FROM
+	users;
 
 -- 2. Ver el registro cuyo id sea igual a 10
-
+SELECT
+	*
+FROM
+	users
+WHERE
+	id = 10;
 
 -- 3. Quiero todos los registros que cuyo primer nombre sea Jim (engañosa)
-
+SELECT
+	*
+FROM
+	users
+WHERE
+	name LIKE 'Jim %'; --respeta el espacio despues de Jim, para asegurarnos que es el primer nombre y no el segundo o tercer nombre
 
 -- 4. Todos los registros cuyo segundo nombre es Alexander
-
+SELECT
+	*
+FROM
+	users
+WHERE
+	name LIKE '% Alexander'; -- el % al inicio indica que puede haber cualquier cosa antes de Alexander, pero debe terminar con Alexander
 
 -- 5. Cambiar el nombre del registro con id = 1, por tu nombre Ej:'Fernando Herrera'
+UPDATE
+	users
+SET
+	NAME = 'Fernando Herrera'
+WHERE
+	id = 1;
 
 
--- 6. Borrar el último registro de la tabla
+-- 6. Borrar el último registro de la tabla 
+SELECT max(id) from users; -- traemos el id mas alto, que es el ultimo registro insertado
+
+DELETE FROM users
+WHERE id = ( SELECT max(id) from users ); -- tambien es posible hacer una subconsulta dentro del delete, para traer el id mas alto y eliminar ese registro
+
+select * from users;
