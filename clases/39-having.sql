4e40f14359e369a2e8cda1dc7c5e335f728b526c
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2026-05-09 19:59:28 -0600

    Add SQL queries for counting users by country and retrieving distinct countries

---
+++ clases/39-having.sql
@@ -0,0 +1,15 @@
+SELECT
+    count(*) as total, -- cuenta cuántas filas hay en cada grupo
+    country   -- muestra el país del grupo
+FROM
+    users     -- tabla users de donde salen los datos
+GROUP BY
+    country  -- agrupa las filas que tengan el mismo país
+HAVING COUNT(*) BETWEEN 1 AND 5 -- having es una condicion ahpra no trae las cantidades mayores a 5
+ORDER BY country DESC;
+
+-- Significado completo de la consulta:
+-- 1. Obtiene los datos de la tabla users
+-- 2. Agrupa los usuarios según el país
+-- 3. Cuenta cuántos usuarios existen en cada país
+-- 4. Devuelve el total de usuarios junto al nombre del país
