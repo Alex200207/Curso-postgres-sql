013bdf7edb9bce7ad1fd1f757eb0d1ece30f2839
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2026-05-11 18:48:14 -0600

    Add SQL script to define primary key and check constraint for country table

---
+++ clases/constrain-check.sql
@@ -0,0 +1,15 @@
+-- BD 1-country.sql
+
+--alter no sirve para modificar la tabla
+-- add para agregar un nueva columna o campo
+-- PRIMARY KEY llave primaria 
+-- el () indica la columna o campo que servira como llave primaria tambien es posible agregar mas para hacer una llave compuesta serparando por comas.
+
+ALTER TABLE country
+add PRIMARY KEY (code);
+
+select * from country;
+
+-- CHECK es una restriccion que se utiliza para limitar los valores que se pueden insertar en una columna o campo, en este caso se esta limitando a que el valor de surfacearea sea mayor o igual a 0.
+alter table country add check(surfacearea >= 0);
+-- resive dentro de parentesis los nombres de las columnas o campos que se van a utilizar para la restriccion, en este caso se esta utilizando la columna surfacearea y se esta limitando a que el valor sea mayor o igual a 0.
