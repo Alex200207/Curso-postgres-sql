d8f4b4e30e0eb24e0a39539e6fea2a963626d44b
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2026-05-14 17:25:40 -0600

    add new concepts

---
+++ clases/56-creando-indices.sql
@@ -0,0 +1,7 @@
+select * from country where continent = 'North America';
+
+-- creacion de indices
+-- podemos tener un indice compuesto y unicos
+create  index "contry_continent" on country (continent);
+
+--crear el índice en la tabla country usando la columna continent
