49c8bfc6258a8f91071d5af3f9bbddbb29cc2759
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2026-05-30 18:46:14 -0600

    add new class

---
+++ clases/76-clausula-UNION.sql
@@ -0,0 +1,10 @@
+-- la UNION es una forma de unir resultados, es sumamente util cuando hay varios resultados que queremos anadir en un solo resultset porque puede ser que queramos hacer un reporte o hacer algo en particular que requiera hacer dos queries
+
+
+
+-- unimos los resultados siempre y cuando tenga mismo numero de columnas
+-- los tipos deben hacer matchs 
+SELECT * from continent WHERE name like '%America%'
+union
+SELECT * FROM continent where code in (3,5)
+order BY "name" ASC;
